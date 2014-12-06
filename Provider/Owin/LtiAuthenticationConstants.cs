4cd2c2758946d4fd4cedee313ef5609d4376f803
amiller97229 <aamiller@alumni.princeton.edu>
2014-12-05 22:39:58 -0800

    Remove old files from Provider

--- Provider/Owin/LtiAuthenticationConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Provider.Owin
-{
-    public static class LtiAuthenticationConstants
-    {
-        public const string LtiAuthenticationType = "LTI";
-        public const string LtiRequestIdUrn = "urn:LtiRequestId";
-    }
-}

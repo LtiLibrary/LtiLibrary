a5dae63f54ba0fceb664f11d2d8b1cb51ee1d7cb
amiller97229 <aamiller@alumni.princeton.edu>
2014-12-14 15:57:15 -0800

    Add AuthorizationHeader class

---
+++ LtiLibrary.Owin.Security.Lti/AuthorizationHeader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Specialized;
+
+namespace LtiLibrary.Owin.Security.Lti
+{
+    public class AuthorizationHeader
+    {
+        public AuthorizationHeader()
+        {
+            Parameters = new NameValueCollection();
+        }
+
+        public string Scheme { get; set; }
+        public NameValueCollection Parameters { get; private set; }
+    }
+}

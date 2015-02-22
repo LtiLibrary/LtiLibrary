52341b2f6667a60bbbd731656293acc24a356e33
andyfmiller <aamiller@alumni.princeton.edu>
2015-02-22 09:11:55 -0800

    Updated assembly description to include LTI Outcomes Management 1.0 and Content-Item Message 1.0. And updated the copyright year.

--- LtiLibrary.AspNet/Properties/AssemblyInfo.cs
+++ LtiLibrary.AspNet/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("LtiLibrary.AspNet")]
-[assembly: AssemblyDescription("ASP.NET library with IMS LTI support for Tool Consumer and Tool Provider applications. Supports IMS LTI 1.0, 1.1, 1.1.1 and 1.2.")]
+[assembly: AssemblyDescription("ASP.NET library with IMS LTI support for Tool Consumer and Tool Provider applications. Supports IMS LTI 1.0, 1.1, 1.1.1 and 1.2; LTI Outcomes Management 1.0; and Content-Item Message 1.0.")]
 [assembly: AssemblyCompany("andyfmiller.com")]
 [assembly: AssemblyProduct("LtiLibrary")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyVersion("1.5.*")]
 [assembly: AssemblyInformationalVersion("1.5.0-beta")]

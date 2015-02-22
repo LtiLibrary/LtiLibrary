2c0e9cbc7e629b6e3b5f3abc3e4ae62b492fb6cc
andyfmiller <aamiller@alumni.princeton.edu>
2015-02-22 09:27:33 -0800

    Started a unit test project.

---
+++ LtiLibrary.AspNet.Tests/LineItemsControllerUnitTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using LtiLibrary.Core.Outcomes.v2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace LtiLibrary.AspNet.Tests
+{
+    [TestClass]
+    public class LineItemsControllerUnitTests
+    {
+        [TestMethod]
+        public void GetLineItemBeforePostReturnsNotFound()
+        {
+            var controller = new LineItemsController();
+            ControllerSetup.RegisterContext(controller, "LineItems");
+            var result = controller.Get(LineItemsController.LineItemId);
+            Assert.AreEqual(HttpStatusCode.NotFound, result.Result.StatusCode);
+        }
+
+        [TestMethod]
+        public void PostLineItemReturnsValidLineItem()
+        {
+            var controller = new LineItemsController();
+            ControllerSetup.RegisterContext(controller, "LineItems");
+            var lineitem = new LineItem
+            {
+                LineItemOf = new Context {  ContextId = LineItemsController.ContextId },
+                ReportingMethod = new Uri("res:Result")
+            };
+            var result = controller.Post(lineitem);
+            Assert.AreEqual(HttpStatusCode.Created, result.Result.StatusCode);
+            var lineItem = JsonConvert.DeserializeObject<LineItem>(result.Result.Content.ReadAsStringAsync().Result);
+            Assert.IsNotNull(lineItem);
+            Assert.AreEqual(LineItemsController.LineItemId, lineItem.Id);
+        }
+
+        [TestMethod]
+        public void GetLineItemsBeforePostReturnsNotFound()
+        {
+            var controller = new LineItemsController();
+            ControllerSetup.RegisterContext(controller, "LineItems");
+            var result = controller.Get();
+            Assert.AreEqual(HttpStatusCode.NotFound, result.Result.StatusCode);
+        }
+    }
+}

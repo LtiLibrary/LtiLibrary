4eeaeb4679c264ba96606c8762c59cb49bdd8b5d
andyfmiller <aamiller@alumni.princeton.edu>
2015-02-22 09:16:06 -0800

    Simplified formatter to matach LineItemsFormatter.

--- LtiLibrary.AspNet/Profiles/ToolConsumerProfileFormatter.cs
+++ LtiLibrary.AspNet/Profiles/ToolConsumerProfileFormatter.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Net.Http.Formatting;
+﻿using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
 using LtiLibrary.Core.Common;
 using Newtonsoft.Json;
@@ -8,30 +7,17 @@ namespace LtiLibrary.AspNet.Profiles
 {
     public class ToolConsumerProfileFormatter : JsonMediaTypeFormatter
     {
-        private static readonly MediaTypeHeaderValue ToolConsumerProfileMediaType 
-            = new MediaTypeHeaderValue(LtiConstants.ToolConsumerProfileMediaType);
-
         public ToolConsumerProfileFormatter()
         {
+            // Accept ToolConsumerProfile JSON-LD
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue(LtiConstants.ToolConsumerProfileMediaType));
+
+            // Tool Providers do not expect null values
+            SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
 #if DEBUG
             // Makes it a little easier to read responses during debug and test
             Indent = true;
 #endif
-
-            // Tool Providers do not expect null values
-            SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
-
-            // Use the "Accept" header to invoke this formatter. If the request accepts the 
-            // ToolConsumerProfileMediaType, then this formatter kicks into action.
-            this.AddRequestHeaderMapping("Accept", LtiConstants.ToolConsumerProfileMediaType,
-                StringComparison.OrdinalIgnoreCase, true, ToolConsumerProfileMediaType);
-        }
-
-        public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
-        {
-            // Replace the default Content-Type header ("application/json") with the ToolConsumerProfileMediaType
-            // defined by IMS. This will also set the Content-Type header in the response.
-            base.SetDefaultContentHeaders(type, headers, ToolConsumerProfileMediaType);
         }
     }
 }

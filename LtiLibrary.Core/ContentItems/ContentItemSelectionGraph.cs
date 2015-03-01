645ad01353cea55a82ccf59e7e76656712c10012
andyfmiller <aamiller@alumni.princeton.edu>
2015-03-01 10:57:24 -0800

    Renamed ContentItemPlacementResponse to ContentItemSelectionGraph.

---
+++ LtiLibrary.Core/ContentItems/ContentItemSelectionGraph.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using LtiLibrary.Core.Common;
+using Newtonsoft.Json;
+
+namespace LtiLibrary.Core.ContentItems
+{
+    public class ContentItemSelectionGraph : JsonLdObject
+    {
+        public ContentItemSelectionGraph() : base(null)
+        {
+            Context = LtiConstants.ContentItemContext;
+        }
+
+        [JsonProperty("@graph")]
+        public IEnumerable<ContentItem> Graph { get; set; }
+    }
+}

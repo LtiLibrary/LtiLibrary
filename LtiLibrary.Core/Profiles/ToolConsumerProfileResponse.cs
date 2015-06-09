0a6f5ae7d523ef2aaab7215d9a80cf757225c077
andyfmiller <aamiller@alumni.princeton.edu>
2015-06-08 21:45:31 -0700

    Removed HttpRequest and HttpResponse strings. Use tracing to debug request/response.

--- LtiLibrary.Core/Profiles/ToolConsumerProfileResponse.cs
+++ LtiLibrary.Core/Profiles/ToolConsumerProfileResponse.cs
@@ -6,16 +6,6 @@ namespace LtiLibrary.Core.Profiles
     {
         public HttpStatusCode StatusCode { get; set; }
 
-        /// <summary>
-        /// String representation of the HttpWebRequest similar to Fiddler's.
-        /// </summary>
-        public string HttpRequest { get; set; }
-
-        /// <summary>
-        /// String representation of the HttpWebResponse similar to Fiddler's.
-        /// </summary>
-        public string HttpResponse { get; set; }
-
         public ToolConsumerProfile ToolConsumerProfile { get; set; }
     }
 }

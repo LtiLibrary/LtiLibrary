b776162d890e4238273ba3d7cfa4c3626592acd3
andyfmiller <aamiller@alumni.princeton.edu>
2015-06-08 21:31:55 -0700

    Add LtiLink ContentItem type.

---
+++ LtiLibrary.Core/ContentItems/LtiLink.cs
@@ -0,0 +1,13 @@
+﻿using LtiLibrary.Core.Common;
+
+namespace LtiLibrary.Core.ContentItems
+{
+    public class LtiLink : ContentItem
+    {
+        public LtiLink()
+        {
+            Type = LtiConstants.LtiLinkType;
+            MediaType = LtiConstants.LtiLinkMediaType;
+        }
+    }
+}

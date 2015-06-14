42905b4e3a70e43b6b9448947146cb35b3cf4d21
andyfmiller <aamiller@alumni.princeton.edu>
2015-06-14 08:48:13 -0700

    Initial push of version 1.6.

--- LtiLibrary.Core.Tests/Properties/AssemblyInfo.cs
+++ LtiLibrary.Core.Tests/Properties/AssemblyInfo.cs
@@ -1,8 +1,9 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("LtiLibrary.Core.Tests")]
-[assembly: AssemblyDescription(".NET library with IMS LTI support for Tool Consumer and Tool Provider applications. Supports IMS LTI 1.0, 1.1, 1.1.1 and 1.2; LTI Outcomes Management 1.0; and Content-Item Message 1.0.")]
+[assembly: AssemblyDescription("Unit tests for LtiLibrary.Core.")]
 [assembly: AssemblyCompany("andyfmiller.com")]
 [assembly: AssemblyProduct("LtiLibrary")]
 [assembly: AssemblyCopyright("Copyright © 2015")]
-[assembly: AssemblyVersion("1.5.*")]
+[assembly: AssemblyVersion("1.6.*")]
+[assembly: AssemblyInformationalVersion("1.6.0")]
--- LtiLibrary.Core/Properties/AssemblyInfo.cs
+++ LtiLibrary.Core/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@
 [assembly: AssemblyCompany("andyfmiller.com")]
 [assembly: AssemblyProduct("LtiLibrary")]
 [assembly: AssemblyCopyright("Copyright © 2015")]
-[assembly: AssemblyVersion("1.5.*")]
-[assembly: AssemblyInformationalVersion("1.5.4")]
+[assembly: AssemblyVersion("1.6.*")]
+[assembly: AssemblyInformationalVersion("1.6.0")]

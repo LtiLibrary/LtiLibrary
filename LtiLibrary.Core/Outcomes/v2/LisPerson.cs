4dc611b0b4d392f835670ab9281a47ad7755242f
andyfmiller <aamiller@alumni.princeton.edu>
2016-03-07 21:07:15 -0800

    Add LISPerson to project.

---
+++ LtiLibrary.Core/Outcomes/v2/LisPerson.cs
@@ -0,0 +1,14 @@
+﻿using LtiLibrary.Core.Common;
+using Newtonsoft.Json;
+
+namespace LtiLibrary.Core.Outcomes.v2
+{
+    public class LisPerson : JsonLdObject
+    {
+        /// <summary>
+        /// A unique identifier for the person.
+        /// </summary>
+        [JsonProperty("userId")]
+        public string UserId { get; set; }
+    }
+}

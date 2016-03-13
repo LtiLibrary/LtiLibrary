6800ee13b0f8f110638d380d8d0429aa16d5362b
andyfmiller <aamiller@alumni.princeton.edu>
2016-03-13 09:50:17 -0700

    Document LineItemsControllerBase.

--- LtiLibrary.AspNet/Outcomes/v2/LineItemsControllerBase.cs
+++ LtiLibrary.AspNet/Outcomes/v2/LineItemsControllerBase.cs
@@ -7,6 +7,10 @@ using LtiLibrary.Core.Outcomes.v2;
 
 namespace LtiLibrary.AspNet.Outcomes.v2
 {
+    /// <summary>
+    /// Based on A REST API for LineItem Resources in multiple formats, Internal Draft 2.1
+    /// https://www.imsglobal.org/lti/model/uml/purl.imsglobal.org/vocab/lis/v2/outcomes/LineItem/service.html
+    /// </summary>
     [LineItemsControllerConfig]
     public abstract class LineItemsControllerBase : ApiController
     {
@@ -19,12 +23,30 @@ namespace LtiLibrary.AspNet.Outcomes.v2
             OnPutLineItem = context => { throw new NotImplementedException(); };
         }
 
+        /// <summary>
+        /// Delete a particular LineItem instance in the Tool Consumer application.
+        /// </summary>
         public Func<DeleteLineItemContext, Task> OnDeleteLineItem { get; set; }
+        /// <summary>
+        /// Get a representation of a particular LineItem instance from the Tool Consumer application.
+        /// </summary>
         public Func<GetLineItemContext, Task> OnGetLineItem { get; set; }
+        /// <summary>
+        /// Get a paginated list of LineItem resources from a LineItemContainer in the Tool Consumer application.
+        /// </summary>
         public Func<GetLineItemsContext, Task> OnGetLineItems { get; set; }
+        /// <summary>
+        /// Create a new LineItem instance within the Tool Consumer Application.
+        /// </summary>
         public Func<PostLineItemContext, Task> OnPostLineItem { get; set; }
+        /// <summary>
+        /// Update a particular LineItem instance in the Tool Consumer Application.
+        /// </summary>
         public Func<PutLineItemContext, Task> OnPutLineItem { get; set; }
 
+        /// <summary>
+        /// Delete a particular LineItem instance.
+        /// </summary>
         [HttpDelete]
         public async Task<HttpResponseMessage> Delete(string id)
         {
@@ -42,6 +64,9 @@ namespace LtiLibrary.AspNet.Outcomes.v2
             }
         }
 
+        /// <summary>
+        /// Get a paginated list of LineItem resources from a LineItemContainer, or get a representation of a particular LineItem instance.
+        /// </summary>
         [HttpGet]
         public async Task<HttpResponseMessage> Get(string id = null, int? limit = null, string activityId = null, string firstPage = null, int? p = null)
         {
@@ -49,6 +74,8 @@ namespace LtiLibrary.AspNet.Outcomes.v2
             {
                 if (string.IsNullOrEmpty(id))
                 {
+                    // Get a paginated list of LineItem resources from a LineItemContainer
+
                     int page = 1;
                     if (p.HasValue)
                     {
@@ -69,6 +96,8 @@ namespace LtiLibrary.AspNet.Outcomes.v2
                 }
                 else
                 {
+                    // Get a representation of a particular LineItem instance
+
                     var context = new GetLineItemContext(id);
 
                     await OnGetLineItem(context);
@@ -84,6 +113,9 @@ namespace LtiLibrary.AspNet.Outcomes.v2
             }
         }
 
+        /// <summary>
+        /// Create a new LineItem instance.
+        /// </summary>
         [HttpPost]
         public async Task<HttpResponseMessage> Post(LineItem lineItem)
         {
@@ -103,6 +135,9 @@ namespace LtiLibrary.AspNet.Outcomes.v2
             }
         }
 
+        /// <summary>
+        /// Update a particular LineItem instance.
+        /// </summary>
         [HttpPut]
         public async Task<HttpResponseMessage> Put(LineItem lineItem)
         {

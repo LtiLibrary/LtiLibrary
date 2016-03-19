722acd98f7a5b70c27304f10171921f36ae88399
andyfmiller <aamiller@alumni.princeton.edu>
2016-03-19 11:55:07 -0700

    Add ids from service endpoint

--- LtiLibrary.AspNet/Outcomes/v2/PutResultContext.cs
+++ LtiLibrary.AspNet/Outcomes/v2/PutResultContext.cs
@@ -5,12 +5,18 @@ namespace LtiLibrary.AspNet.Outcomes.v2
 {
     public class PutResultContext
     {
-        public PutResultContext(LisResult result)
+        public PutResultContext(string contextId, string lineItemId, string id, LisResult result)
         {
+            ContextId = contextId;
+            LineItemId = lineItemId;
+            Id = id;
             Result = result;
             StatusCode = HttpStatusCode.OK;
         }
 
+        public string ContextId { get; set; }
+        public string LineItemId { get; set; }
+        public string Id { get; set; }
         public LisResult Result { get; private set; }
         public HttpStatusCode StatusCode { get; set; }
     }

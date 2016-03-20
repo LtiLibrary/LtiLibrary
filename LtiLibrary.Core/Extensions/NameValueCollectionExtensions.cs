3199062232c6e2990827ec45bb1b4337b4eda1aa
andyfmiller <aamiller@alumni.princeton.edu>
2016-03-20 12:22:59 -0700

    Added documentation

--- LtiLibrary.Core/Extensions/NameValueCollectionExtensions.cs
+++ LtiLibrary.Core/Extensions/NameValueCollectionExtensions.cs
@@ -54,11 +54,20 @@ namespace LtiLibrary.Core.Extensions
             }
         }
 
+        /// <summary>
+        /// Return a normalized string of parameters suitable for OAuth 1.0 signature base string
+        /// as defined by https://tools.ietf.org/html/rfc5849#section-3.4.1.3.2
+        /// </summary>
+        /// <param name="collection">The list of name/value pairs to normalize.</param>
+        /// <param name="excludedNames">List of names that must be excluded from the signature base string.</param>
+        /// <returns>A normalized string of parameters.</returns>
         public static string ToNormalizedString(this NameValueCollection collection, IList<string> excludedNames = null)
         {
             var list = new List<KeyValuePair<string, string>>();
 
-// ReSharper disable once LoopCanBeConvertedToQuery
+            // ReSharper disable once LoopCanBeConvertedToQuery
+            // https://tools.ietf.org/html/rfc5849#section-3.4.1.3.1
+            // Exclude excluded parameters (e.g. oauth_signature)
             foreach (var key in collection.AllKeys)
             {
                 if (excludedNames != null && excludedNames.Contains(key)) continue;
@@ -67,6 +76,8 @@ namespace LtiLibrary.Core.Extensions
                     value.ToRfc3986EncodedString()));
             }
 
+            // https://tools.ietf.org/html/rfc5849#section-3.4.1.3.2
+            // Sort the parameters by name, then value
             list.Sort((left, right) => left.Key.Equals(right.Key, StringComparison.Ordinal)
                 ? string.Compare(left.Value, right.Value, StringComparison.Ordinal)
                 : string.Compare(left.Key, right.Key, StringComparison.Ordinal));

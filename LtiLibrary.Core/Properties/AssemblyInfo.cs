7ee104f52f6fdf1ae81b5a6f2f93f2f3409fac7f
andyfmiller <aamiller@alumni.princeton.edu>
2016-03-21 21:06:56 -0700

    Version 1.6

--- LtiLibrary.Core/Properties/AssemblyInfo.cs
+++ LtiLibrary.Core/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("LtiLibrary.Core")]
-[assembly: AssemblyDescription(".NET library with IMS LTI support for Tool Consumer and Tool Provider applications. Supports IMS LTI 1.0, 1.1, 1.1.1 and 1.2; LTI Outcomes Management 1.0; and Content-Item Message 1.0.")]
+[assembly: AssemblyDescription(".NET library with IMS LTI support for Tool Consumer and Tool Provider applications. Supports IMS LTI 1.0, 1.1, 1.1.1 and 1.2; Outcomes 1.0; Outcomes 2.0 (Draft); and Content-Item Message 1.0.")]
 [assembly: AssemblyCompany("andyfmiller.com")]
 [assembly: AssemblyProduct("LtiLibrary")]
-[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyVersion("1.6.*")]
 [assembly: AssemblyInformationalVersion("1.6.0")]

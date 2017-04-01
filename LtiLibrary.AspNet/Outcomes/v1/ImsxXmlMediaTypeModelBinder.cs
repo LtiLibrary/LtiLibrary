8527b470e810059d36fccf6979ef6582e09d5e77
Andy Miller <andy@andyfmiller.com>
2017-04-01 11:20:47 -0700

    Add comments, better error handling to new class.

--- LtiLibrary.AspNet/Outcomes/v1/ImsxXmlMediaTypeModelBinder.cs
+++ LtiLibrary.AspNet/Outcomes/v1/ImsxXmlMediaTypeModelBinder.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using LtiLibrary.Core.Outcomes.v1;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
-using LtiLibrary.Core.Outcomes.v1;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-using System.IO;
 
 namespace LtiLibrary.AspNet.Outcomes.v1
 {
+    /// <summary>
+    /// Use this ModelBinder to deserialize imsx_POXEnvelopeRequest XML. For example,
+    /// <code>public ImsxXmlMediaTypeResult Post([ModelBinder(BinderType = typeof(ImsxXmlMediaTypeModelBinder))] imsx_POXEnvelopeType request)</code>
+    /// </summary>
     public class ImsxXmlMediaTypeModelBinder : IModelBinder
     {
         private static readonly XmlSerializer ImsxRequestSerializer = new XmlSerializer(typeof(imsx_POXEnvelopeType),
@@ -17,10 +21,24 @@ namespace LtiLibrary.AspNet.Outcomes.v1
         {
             if (bindingContext == null) throw new ArgumentNullException(nameof(bindingContext));
 
-            using (var reader = new StreamReader(bindingContext.HttpContext.Request.Body))
+            if (bindingContext.HttpContext?.Request == null || !bindingContext.HttpContext.Request.ContentType.Equals("application/xml"))
             {
-                var model = ImsxRequestSerializer.Deserialize(reader);
-                bindingContext.Result = ModelBindingResult.Success(model);
+                bindingContext.Result = ModelBindingResult.Failed();
+            }
+            else
+            {
+                try
+                {
+                    using (var reader = new StreamReader(bindingContext.HttpContext.Request.Body))
+                    {
+                        var model = ImsxRequestSerializer.Deserialize(reader);
+                        bindingContext.Result = ModelBindingResult.Success(model);
+                    }
+                }
+                catch
+                {
+                    bindingContext.Result = ModelBindingResult.Failed();
+                }
             }
 
             return null;

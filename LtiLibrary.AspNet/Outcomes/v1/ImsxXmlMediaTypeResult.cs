d60769ccbffc31769b2f2cb19104e2dcb2c25f23
Andy Miller <andy@andyfmiller.com>
2017-04-01 11:23:44 -0700

    Add comments to a new class.

--- LtiLibrary.AspNet/Outcomes/v1/ImsxXmlMediaTypeResult.cs
+++ LtiLibrary.AspNet/Outcomes/v1/ImsxXmlMediaTypeResult.cs
@@ -2,10 +2,15 @@
 
 namespace LtiLibrary.AspNet.Outcomes.v1
 {
+    /// <summary>
+    /// Use to attach a specific XML formatter to controller results. For example,
+    /// <code>public ImsxXmlMediaTypeResult Post([ModelBinder(BinderType = typeof(ImsxXmlMediaTypeModelBinder))] imsx_POXEnvelopeType request)</code>
+    /// </summary>
     public class ImsxXmlMediaTypeResult : ObjectResult
     {
         public ImsxXmlMediaTypeResult(object value) : base(value)
         {
+            // This formatter produces an imsx_POXEnvelopeType with an imsx_POXEnvelopeResponse
             Formatters.Add(new ImsxXmlMediaTypeOutputFormatter());
         }
     }

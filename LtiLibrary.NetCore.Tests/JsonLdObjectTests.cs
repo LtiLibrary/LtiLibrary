c9132c54177e961cc0b02c1c9d33eb872c5ca8c1
Andy Miller <andy@andyfmiller.com>
2017-04-03 18:16:57 -0700

    Add LtiLibrary.NetCore.Tests (based on LtiLibrary.Core.Tests).

---
+++ LtiLibrary.NetCore.Tests/JsonLdObjectTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using LtiLibrary.NetCore.Common;
+using LtiLibrary.NetCore.Tests.SimpleHelpers;
+using Xunit;
+
+namespace LtiLibrary.NetCore.Tests
+{
+    public class JsonLdObjectTests
+    {
+        [Fact]
+        public void ExternalAndInternalContexts_MatchesReferenceJson()
+        {
+            var parent = new Parent
+            {
+                Id = new Uri("http://localhost/test/1"),
+                Name = "MyParent",
+                Child = new Child
+                {
+                    Name = "MyChild",
+                    GrandChild = new Child
+                    {
+                        Name = "MyGrandChild"
+                    }
+                }
+            };
+            JsonAssertions.AssertSameJsonLdObject(parent, "ExternalAndInternalContexts");
+        }
+
+        [Fact]
+        public void ExternalContextOnly_MatchesReferenceJson()
+        {
+            var parent = new Parent
+            {
+                Id = new Uri("http://localhost/test/1"),
+                Name = "MyParent",
+            };
+            JsonAssertions.AssertSameJsonLdObject(parent, "ExternalContextOnly");
+        }
+
+        [Fact]
+        public void ExternalContextOnly_ToJsonLdStringMatchesReferenceJson()
+        {
+            var parent = new Parent
+            {
+                Id = new Uri("http://localhost/test/1"),
+                Name = "MyParent",
+            };
+            JsonAssertions.AssertSameJsonLdObject(parent, "ExternalContextOnly");
+        }
+
+        [Fact]
+        public void InternalContextOnly_MatchesReferenceJson()
+        {
+            var child = new Child
+            {
+                Name = "MyChild",
+                GrandChild = new Child
+                {
+                    Name = "MyGrandChild"
+                }
+            };
+            JsonAssertions.AssertSameJsonLdObject(child, "InternalContextOnly");
+        }
+    }
+
+    public class Parent : JsonLdObject
+    {
+        public Parent()
+        {
+            ExternalContextId = new Uri("http://localhost/ParentContext");
+            Type = "Parent";
+        }
+
+        public Child Child { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class Child : JsonLdObject
+    {
+        public Child()
+        {
+            Terms.Add("Child", "ChildTerms");
+            Type = "Child";
+        }
+
+        public string Name { get; set; }
+
+        public Child GrandChild { get; set; }
+    }
+}
---
+++ LtiLibrary.NetCore.Tests/SimpleHelpers/JsonAssertions.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using LtiLibrary.NetCore.Common;
+using LtiLibrary.NetCore.Extensions;
+using LtiLibrary.NetCore.Tests.TestHelpers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NodaTime;
+using NodaTime.Serialization.JsonNet;
+using Xunit;
+
+namespace LtiLibrary.NetCore.Tests.SimpleHelpers
+{
+        internal static class JsonAssertions
+        {
+            private static readonly JsonSerializerSettings SerializerSettings;
+
+            static JsonAssertions()
+            {
+                SerializerSettings = new JsonSerializerSettings();
+                SerializerSettings.ConfigureForNodaTime(DateTimeZoneProviders.Tzdb);
+            }
+
+            public static void AssertSameObjectJson(object obj, string eventReferenceFile)
+            {
+                var eventJsonString = JsonConvert.SerializeObject(obj, SerializerSettings);
+                var eventJObject = JObject.Parse(eventJsonString);
+                var refJsonString = TestUtils.LoadReferenceJsonFile(eventReferenceFile);
+                var refJObject = JObject.Parse(refJsonString);
+
+                var diff = ObjectDiffPatch.GenerateDiff(refJObject, eventJObject);
+
+                Debug.WriteLine(diff.NewValues);
+                Debug.WriteLine(diff.OldValues);
+
+                Assert.Null(diff.NewValues);
+                Assert.Null(diff.OldValues);
+            }
+
+            public static void AssertSameJsonLdObject( JsonLdObject obj, string eventReferenceFile )
+            {
+               var eventJsonString = obj.ToJsonLdString();
+               var eventJObject = JObject.Parse( eventJsonString );
+               var refJsonString = TestUtils.LoadReferenceJsonFile( eventReferenceFile );
+               var refJObject = JObject.Parse( refJsonString );
+
+               var diff = ObjectDiffPatch.GenerateDiff( refJObject, eventJObject );
+
+               Debug.WriteLine( diff.NewValues );
+               Debug.WriteLine( diff.OldValues );
+
+               Assert.Null( diff.NewValues );
+               Assert.Null( diff.OldValues );
+            }
+        }
+}

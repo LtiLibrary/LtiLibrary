2f7f0d7f1309a62d68edf5d0a59ff569b0246d07
Andy Miller <andy@andyfmiller.com>
2017-04-05 20:21:12 -0700

    Add LtiLibrary.AspNetCore.Tests for integration tests.

---
+++ LtiLibrary.AspNetCore.Tests/Outcomes/v2/LineItemsController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using LtiLibrary.AspNetCore.Outcomes.v2;
+using LtiLibrary.NetCore.Common;
+using LtiLibrary.NetCore.Outcomes.v2;
+using Microsoft.AspNetCore.Http;
+
+namespace LtiLibrary.AspNetCore.Tests.Outcomes.v2
+{
+    public class LineItemsController : LineItemsControllerBase
+    {
+        // Simple "database" of lineitems for demonstration purposes
+        public const string ContextId = "course-1";
+        public const string LineItemId = "lineitem-1";
+        public const string ResultId = "result-1";
+        public static LineItem LineItem;
+        public static LisResult Result;
+
+        public LineItemsController()
+        {
+            OnDeleteLineItem = context =>
+            {
+                var lineItemUri = new Uri(Url.Link("LineItemsApi", new { ContextId, context.Id }));
+
+                if (LineItem == null || !LineItem.Id.Equals(lineItemUri))
+                {
+                    context.StatusCode = StatusCodes.Status404NotFound;
+                }
+                else
+                {
+                    LineItem = null;
+                    Result = null;
+                    context.StatusCode = StatusCodes.Status200OK;
+                }
+                return Task.FromResult<object>(null);
+            };
+
+            OnGetLineItem = context =>
+            {
+                var lineItemUri = new Uri(Url.Link("LineItemsApi", new { ContextId, context.Id }));
+
+                if (LineItem == null || !LineItem.Id.Equals(lineItemUri))
+                {
+                    context.StatusCode = StatusCodes.Status404NotFound;
+                }
+                else
+                {
+                    context.LineItem = LineItem;
+                    context.StatusCode = StatusCodes.Status200OK;
+                }
+                return Task.FromResult<object>(null);
+            };
+
+            OnGetLineItemWithResults = context =>
+            {
+                OnGetLineItem(context);
+                if (context.LineItem != null && Result != null)
+                {
+                    context.LineItem.Result = Result == null ? new LisResult[] { } : new[] { Result };
+                }
+                return Task.FromResult<object>(null);
+            };
+
+            OnGetLineItems = context =>
+            {
+                context.LineItemContainerPage = new LineItemContainerPage
+                {
+                    ExternalContextId = LtiConstants.LineItemContainerContextId,
+                    Id = new Uri(Url.Link("LineItemsApi", new { context.ContextId })),
+                    LineItemContainer = new LineItemContainer
+                    {
+                        MembershipSubject = new LineItemMembershipSubject
+                        {
+                            ContextId = context.ContextId,
+                            LineItems = new LineItem[] { }
+                        }
+                    }
+                };
+
+                if (LineItem != null &&
+                    (string.IsNullOrEmpty(context.ActivityId) ||
+                     context.ActivityId.Equals(LineItem.AssignedActivity.ActivityId)))
+                {
+                    context.LineItemContainerPage.LineItemContainer.MembershipSubject.LineItems = new[] { LineItem };
+                }
+                context.StatusCode = StatusCodes.Status200OK;
+                return Task.FromResult<object>(null);
+            };
+
+            // Create a LineItem
+            OnPostLineItem = context =>
+            {
+                if (LineItem != null)
+                {
+                    context.StatusCode = StatusCodes.Status400BadRequest;
+                    return Task.FromResult<object>(null);
+                }
+
+                context.LineItem.Id = new Uri(Url.Link("LineItemsApi", new { context.ContextId, id = LineItemId }));
+                context.LineItem.Results = new Uri(Url.Link("ResultsApi", new { context.ContextId, LineItemId }));
+                LineItem = context.LineItem;
+                context.StatusCode = StatusCodes.Status201Created;
+                return Task.FromResult<object>(null);
+            };
+
+            // Update LineItem (but not results)
+            OnPutLineItem = context =>
+            {
+                if (context.LineItem == null || LineItem == null || !LineItem.Id.Equals(context.LineItem.Id))
+                {
+                    context.StatusCode = StatusCodes.Status404NotFound;
+                }
+                else
+                {
+                    context.LineItem.Result = LineItem.Result;
+                    LineItem = context.LineItem;
+                    context.StatusCode = StatusCodes.Status200OK;
+                }
+                return Task.FromResult<object>(null);
+            };
+
+            // Update LineItem and Result
+            OnPutLineItemWithResults = context =>
+            {
+                if (context.LineItem == null || LineItem == null || !LineItem.Id.Equals(context.LineItem.Id))
+                {
+                    context.StatusCode = StatusCodes.Status404NotFound;
+                }
+                else
+                {
+                    LineItem = context.LineItem;
+                    if (context.LineItem.Result != null && context.LineItem.Result.Length > 0)
+                    {
+                        Result = context.LineItem.Result[0];
+                    }
+                    context.StatusCode = StatusCodes.Status200OK;
+                }
+                return Task.FromResult<object>(null);
+            };
+        }
+    }
+}
---
+++ LtiLibrary.AspNetCore.Tests/Outcomes/v2/ResultsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using LtiLibrary.AspNetCore.Outcomes.v2;
+using LtiLibrary.NetCore.Common;
+using LtiLibrary.NetCore.Outcomes.v2;
+using Microsoft.AspNetCore.Http;
+
+namespace LtiLibrary.AspNetCore.Tests.Outcomes.v2
+{
+    public class ResultsController : ResultsControllerBase
+    {
+        public ResultsController()
+        {
+            OnDeleteResult = context =>
+            {
+                var resultUri = new Uri(Url.Link("ResultsApi", new { context.ContextId, context.LineItemId, context.Id }));
+
+                if (LineItemsController.Result == null || !LineItemsController.Result.Id.Equals(resultUri))
+                {
+                    context.StatusCode = StatusCodes.Status404NotFound;
+                }
+                else
+                {
+                    LineItemsController.Result = null;
+                    context.StatusCode = StatusCodes.Status200OK;
+                }
+                return Task.FromResult<object>(null);
+            };
+
+            OnGetResult = context =>
+            {
+                // https://www.imsglobal.org/specs/ltiomv2p0/specification-3
+                // When a line item is created, a result for each user is deemed to be created with a status value of “Initialized”.  
+                // Thus, there is no need to actually create a result with a POST request; the first connection to a result may be a PUT or a GET request.
+
+                var lineItemUri = new Uri(Url.Link("LineItemsApi", new { context.ContextId, id = context.LineItemId }));
+                var resultUri = new Uri(Url.Link("ResultsApi", new { context.ContextId, context.LineItemId, context.Id }));
+
+                if (LineItemsController.LineItem == null || !LineItemsController.LineItem.Id.Equals(lineItemUri))
+                {
+                    context.StatusCode = StatusCodes.Status404NotFound;
+                }
+                else if (LineItemsController.Result == null || !LineItemsController.Result.Id.Equals(resultUri))
+                {
+                    context.Result = new LisResult
+                    {
+                        ExternalContextId = LtiConstants.ResultContextId,
+                        Id = resultUri,
+                        ResultOf = lineItemUri,
+                        ResultStatus = ResultStatus.Initialized
+                    };
+                    context.StatusCode = StatusCodes.Status200OK;
+                }
+                else
+                {
+                    context.Result = LineItemsController.Result;
+                    context.StatusCode = StatusCodes.Status200OK;
+                }
+                return Task.FromResult<object>(null);
+            };
+
+            OnGetResults = context =>
+            {
+                var lineItemUri = new Uri(Url.Link("LineItemsApi", new { context.ContextId, id = context.LineItemId }));
+                if (LineItemsController.LineItem == null || !LineItemsController.LineItem.Id.Equals(lineItemUri))
+                {
+                    context.StatusCode = StatusCodes.Status404NotFound;
+                }
+                else
+                {
+                    context.ResultContainerPage = new ResultContainerPage
+                    {
+                        ExternalContextId = LtiConstants.ResultContainerContextId,
+                        Id = new Uri(Url.Link("ResultsApi", new { context.ContextId, context.LineItemId })),
+                        ResultContainer = new ResultContainer
+                        {
+                            MembershipSubject = new ResultMembershipSubject
+                            {
+                                Results = LineItemsController.Result == null ? new LisResult[] { } : new[] { LineItemsController.Result }
+                            }
+                        }
+                    };
+                    context.StatusCode = StatusCodes.Status200OK;
+                }
+                return Task.FromResult<object>(null);
+            };
+
+            OnPostResult = context =>
+            {
+                var lineItemUri = new Uri(Url.Link("LineItemsApi", new { context.ContextId, id = context.LineItemId }));
+                if (LineItemsController.LineItem == null || !LineItemsController.LineItem.Id.Equals(lineItemUri))
+                {
+                    context.StatusCode = StatusCodes.Status400BadRequest;
+                }
+                else
+                {
+                    LineItemsController.Result = context.Result;
+                    LineItemsController.Result.Id = new Uri(Url.Link("ResultsApi", new { context.ContextId, context.LineItemId, id = LineItemsController.ResultId }));
+                    context.Result = LineItemsController.Result;
+                    context.StatusCode = StatusCodes.Status201Created;
+                }
+                return Task.FromResult<object>(null);
+            };
+
+            OnPutResult = context =>
+            {
+                // https://www.imsglobal.org/specs/ltiomv2p0/specification-3
+                // When a line item is created, a result for each user is deemed to be created with a status value of “Initialized”.  
+                // Thus, there is no need to actually create a result with a POST request; the first connection to a result may be a 
+                // PUT or a GET request.
+
+                var lineItemUri = new Uri(Url.Link("LineItemsApi", new { context.ContextId, id = context.LineItemId }));
+                if (LineItemsController.LineItem == null || !LineItemsController.LineItem.Id.Equals(lineItemUri))
+                {
+                    context.StatusCode = StatusCodes.Status404NotFound;
+                }
+                else
+                {
+                    // If this is the first connection, the PUT is equivalent to a POST
+                    LineItemsController.Result = context.Result;
+                    if (context.Result.Id == null)
+                    {
+                        LineItemsController.Result.Id = new Uri(Url.Link("ResultsApi", new { context.ContextId, context.LineItemId, id = LineItemsController.ResultId }));
+                    }
+                    context.StatusCode = StatusCodes.Status200OK;
+                }
+                return Task.FromResult<object>(null);
+            };
+        }
+    }
+}

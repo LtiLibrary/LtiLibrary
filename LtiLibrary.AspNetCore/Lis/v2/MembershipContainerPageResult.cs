74ca48dd3a92519733f27fa0135a14494363d932
Andy Miller <andy@andyfmiller.com>
2017-04-26 20:10:41 -0700

    Add Membership Service 1.0.

---
+++ LtiLibrary.AspNetCore/Lis/v2/MembershipContainerPageResult.cs
@@ -0,0 +1,23 @@
+﻿using LtiLibrary.NetCore.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LtiLibrary.AspNetCore.Lis.v2
+{
+    /// <summary>
+    /// Represents the MembershipContainerPage returned from the LineItemsController to the <see cref="MembershipsControllerBase"/>.
+    /// </summary>
+    public class MembershipContainerPageResult : JsonResult
+    {
+        /// <summary>
+        /// Initializes a new instance of the MembershipContainerPageResult class.
+        /// </summary>
+        /// <param name="value">The object to return.</param>
+        /// <param name="statusCode">The HTTP StatusCode to return.</param>
+        public MembershipContainerPageResult(object value, int statusCode = StatusCodes.Status200OK) : base(value)
+        {
+            ContentType = LtiConstants.LisMembershipContainerMediaType;
+            StatusCode = statusCode;
+        }
+    }
+}
---
+++ LtiLibrary.AspNetCore/Lis/v2/MembershipsControllerBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LtiLibrary.AspNetCore.Common;
+using LtiLibrary.NetCore.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LtiLibrary.AspNetCore.Lis.v2
+{
+    /// <summary>
+    /// A <see cref="Controller" /> that implements 
+    /// "A REST API for LineItem Resources in multiple formats, Internal Draft 2.1"
+    /// https://www.imsglobal.org/lti/model/uml/purl.imsglobal.org/vocab/lis/v2/outcomes/LineItem/service.html
+    /// </summary>
+    [AddBodyHashHeader]
+    [Route("ims/courses/{contextId}/lineitems/{id?}", Name = "LineItemsApi")]
+    [Consumes(LtiConstants.LisLineItemMediaType, LtiConstants.LisLineItemResultsMediaType, LtiConstants.LisLineItemContainerMediaType)]
+    [Produces(LtiConstants.LisLineItemMediaType, LtiConstants.LisLineItemResultsMediaType, LtiConstants.LisLineItemContainerMediaType)]
+    public class MembershipsControllerBase : Controller
+    {
+    }
+}

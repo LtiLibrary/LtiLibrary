7a893ed3bb6e428db78678fa12b55b8621bb5767
Sander Rijken <sr@d-90.nl>
2017-12-11 11:38:47 +0100

    Add missing file

---
+++ src/LtiLibrary.NetCore/OAuth/SignatureMethod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LtiLibrary.NetCore.OAuth
+{
+    public enum SignatureMethod
+    {
+        HmacSha1,
+        HmacSha256,
+        HmacSha384,
+        HmacSha512,
+    }
+}

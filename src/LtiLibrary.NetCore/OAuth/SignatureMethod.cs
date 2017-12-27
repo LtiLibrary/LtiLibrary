772a308b8d1587ab724ec9436ef6ee490d4f6fec
Andy Miller <andy@andyfmiller.com>
2017-12-27 08:23:24 -0800

    Add missing XML comments.

--- src/LtiLibrary.NetCore/OAuth/SignatureMethod.cs
+++ src/LtiLibrary.NetCore/OAuth/SignatureMethod.cs
@@ -1,14 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace LtiLibrary.NetCore.OAuth
+﻿namespace LtiLibrary.NetCore.OAuth
 {
+    /// <summary>
+    /// LTI signature methods for computing hashes.
+    /// </summary>
     public enum SignatureMethod
     {
+        /// <summary>
+        /// Computes the <see cref="System.Security.Cryptography.SHA1"/> hash for the input data.
+        /// This is the default for LTI 1.1.
+        /// </summary>
         HmacSha1,
+
+        /// <summary>
+        /// Computes the <see cref="System.Security.Cryptography.SHA256"/> hash for the input data.
+        /// </summary>
         HmacSha256,
+
+        /// <summary>
+        /// Computes the <see cref="System.Security.Cryptography.SHA384"/> hash for the input data.
+        /// </summary>
         HmacSha384,
+
+        /// <summary>
+        /// Computes the <see cref="System.Security.Cryptography.SHA512"/> hash for the input data.
+        /// </summary>
         HmacSha512,
     }
 }

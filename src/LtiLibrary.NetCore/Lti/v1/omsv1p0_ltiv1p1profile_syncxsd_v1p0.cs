cb847c9de2a301bd55bf7b20edde7bf451d5918a
Skyler Todd <Skyler.Todd@gmail.com>
2018-03-30 08:52:05 -0700

    Reverted changes to autogened xml classess, implemented same functionality through partials in a canvas Extension file.

--- src/LtiLibrary.NetCore/Lti/v1/omsv1p0_ltiv1p1profile_syncxsd_v1p0.cs
+++ src/LtiLibrary.NetCore/Lti/v1/omsv1p0_ltiv1p1profile_syncxsd_v1p0.cs
@@ -358,7 +358,6 @@ namespace LtiLibrary.NetCore.Lti.v1
     {
 
         private TextType resultScoreField;
-        private LtiReponseType resultDataField;
 
         /// <remarks/>
         public TextType resultScore
@@ -370,19 +369,6 @@ namespace LtiLibrary.NetCore.Lti.v1
             set
             {
                 this.resultScoreField = value;
-            }
-        }
-
-        /// <remarks/>
-        public LtiReponseType resultData
-        {
-            get
-            {
-                return this.resultDataField;
-            }
-            set
-            {
-                this.resultDataField = value;
             }
         }
     }
@@ -427,33 +413,6 @@ namespace LtiLibrary.NetCore.Lti.v1
                 this.textStringField = value;
             }
         }
-    }
-    /// <remarks/>
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.0.30319.17929")]
-    [System.SerializableAttribute()]
-    [System.Diagnostics.DebuggerStepThroughAttribute()]
-    [System.ComponentModel.DesignerCategoryAttribute("code")]
-    //[XmlType(TypeName = "Text.Type", Namespace = "http://www.imsglobal.org/services/ltiv1p1/xsd/imsoms_v1p0")]
-    [XmlRoot("resultScore", Namespace = "http://www.imsglobal.org/services/ltiv1p1/xsd/imsoms_v1p0", IsNullable = false)]
-    public partial class LtiReponseType
-    {
-
-        private string ltiLaunchUrlField;
-
-        /// <remarks/>
-        public string ltiLaunchUrl
-        {
-            get
-            {
-                return this.ltiLaunchUrlField;
-            }
-            set
-            {
-                this.ltiLaunchUrlField = value;
-            }
-        }
-
-
     }
 
     /// <remarks/>

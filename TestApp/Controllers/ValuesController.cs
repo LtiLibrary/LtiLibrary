c8af4dc34b956a0382b44cdce7bbf0c8d3636848
Skyler Todd <Skyler.Todd@gmail.com>
2018-03-30 07:57:54 -0700

    working but dirty

---
+++ TestApp/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using LtiLibrary.AspNetCore.Extensions;
+using LtiLibrary.NetCore.Clients;
+using LtiLibrary.NetCore.Lti.v1;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class LtiController : Controller
+    {
+        string key = "jisc.ac.uk";
+        string secret = "secret";
+        static double callCount = 0.0;
+        //GET api/values
+        [HttpPost]
+        public async Task<IEnumerable<string>> Launch()
+        {
+            callCount++;
+
+            LtiRequest ltiReqest = await Request.ParseLtiRequestAsync();
+            var client = new HttpClient();
+            var outcomeUrl = ltiReqest.LisOutcomeServiceUrl;
+            string lisResultSourcedId = ltiReqest.LisResultSourcedId;
+            await Outcomes1ClientCanvas.ReplaceResultWithLtiLaunchResultAsync(client, outcomeUrl, key, secret, lisResultSourcedId, callCount / 10, "https://f51f4fa4.ngrok.io/api/review/");
+
+            return new string[] { "value1", "value2" };
+        }
+    }
+
+    [Route("api/[controller]")]
+    public class ReviewController : Controller
+    {
+        string key = "jisc.ac.uk";
+        string secret = "secret";
+        static double callCount = 0.0;
+        //GET api/values
+        [HttpPost]
+        public async Task<IEnumerable<string>> Review()
+        {
+           
+
+            return new string[] { "review"};
+        }
+    }
+}
